4af1270ab2e59d011f0c86bd8cd4b083bbe272f0
bruhGrassi <brunagm88@gmail.com>
2024-07-15 22:01:46 -0300

    chore: pass current weather to components

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "React";
+import useFetchWeather from "./hooks/useFetchWeather";
 import TodayWeather from "./components/TodayWeather/TodayWeather";
 import Sidebar from "./components/Sidebar/Sidebar";
 import RoundButton from "./components/RoundButton/RoundButton";
@@ -7,95 +8,39 @@ import HighlightCard from "./components/HighlightCard/HighlightCard";
 import "./App.css";
 
 function App() {
+  const {
+    currentWeatherData,
+    forecastWeatherData,
+    error,
+    unit,
+    setUnit,
+    handleTemperatureUnit,
+  } = useFetchWeather("London");
   const [isSidebar, setIsSidebar] = useState(false);
-  const [currentCity, setCurrentCity] = useState("London");
-  const [weatherData, setWeatherData] = useState({});
-  const [error, setError] = useState(null);
-  const [unit, setUnit] = useState("C");
-
-  const API_KEY = "4d7cbd0d9f544d18cd63e774e861a657";
-  const API_URL = `https://api.openweathermap.org/data/2.5/weather`;
-
-  const mapWeatherData = (data) => {
-    return {
-      name: data.name,
-      temp: data.main.temp,
-      description: data.weather[0].main,
-      image: data.weather[0].icon,
-      wind: data.wind.speed,
-      humidity: data.main.humidity,
-      visibility: data.visibility,
-      pressure: data.main.pressure,
-    };
-  };
-
-  const fetchWeather = async (location) => {
-    try {
-      let url;
-
-      if (typeof location === "string") {
-        url = `${API_URL}?q=${location}&appid=${API_KEY}&units=metric`;
-      } else {
-        url = `${API_URL}?lat=${location.lat}&lon=${location.lon}&appid=${API_KEY}&units=metric`;
-      }
-
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("City not found");
-      }
-      const data = await response.json();
-      const mappedData = mapWeatherData(data);
-      setWeatherData(mappedData); //setWeatherData is asynchronous, so the state update is not immediate
-      return data;
-    } catch (error) {
-      console.error("Error fetching weather data:", error);
-      setError(error.message);
-      return null;
-    }
-  };
-
-  useEffect(() => {
-    fetchWeather(currentCity);
-  }, []);
-
-  useEffect(() => {
-    if (weatherData) {
-      console.log("Updated weatherData", weatherData);
-    }
-  }, [weatherData]);
 
   const handleSidebar = () => {
     setIsSidebar(!isSidebar);
   };
 
-  const handleTemperatureUnit = (tempInCelsius, unit) => {
-    if (unit === "C") {
-      return Math.trunc(tempInCelsius);
-    }
-    const tempInFahrenheit = (tempInCelsius * 9) / 5 + 32;
-    return Math.trunc(tempInFahrenheit);
-  };
-
   return (
     <>
       <div>
-        {weatherData ? (
+        {currentWeatherData.name ? (
           <section className="wrapper">
             <aside className="aside">
               {isSidebar ? (
-                <Sidebar
-                  handleSidebar={handleSidebar}
-                  fetchWeather={fetchWeather}
-                />
+                <Sidebar handleSidebar={handleSidebar} />
               ) : (
                 <TodayWeather
-                  cityName={weatherData.name}
-                  temperature={handleTemperatureUnit(weatherData.temp, unit)}
-                  description={weatherData.description}
-                  image={weatherData.image}
+                  cityName={currentWeatherData.name}
+                  temperature={handleTemperatureUnit(
+                    currentWeatherData.temp,
+                    unit
+                  )}
+                  description={currentWeatherData.description}
+                  image={currentWeatherData.image}
                   unit={unit}
                   handleSidebar={handleSidebar}
-                  fetchWeather={fetchWeather}
                 />
               )}
             </aside>
@@ -127,28 +72,30 @@ function App() {
               <p className="main__highlight--text">Today's Highlight</p>
 
               <div className="main__highlight">
-                <HighlightCard
-                  title={"Wind status"}
-                  data={weatherData.wind}
-                  unit={"mph"}
-                  other={true}
-                />
-                <HighlightCard
-                  title={"Humidity"}
-                  data={weatherData.humidity}
-                  unit={"%"}
-                  range={weatherData.humidity}
-                />
-                <HighlightCard
-                  title={"Visibility"}
-                  data={weatherData.visibility}
-                  unit={"miles"}
-                />
-                <HighlightCard
-                  title={"Air Pressure"}
-                  data={weatherData.pressure}
-                  unit={"mb"}
-                />
+                <div className="main__highlight">
+                  <HighlightCard
+                    title={"Wind status"}
+                    data={currentWeatherData.wind}
+                    unit={"mph"}
+                    other={true}
+                  />
+                  <HighlightCard
+                    title={"Humidity"}
+                    data={currentWeatherData.humidity}
+                    unit={"%"}
+                    range={currentWeatherData.humidity}
+                  />
+                  <HighlightCard
+                    title={"Visibility"}
+                    data={currentWeatherData.visibility}
+                    unit={"miles"}
+                  />
+                  <HighlightCard
+                    title={"Air Pressure"}
+                    data={currentWeatherData.pressure}
+                    unit={"mb"}
+                  />
+                </div>
               </div>
             </main>
           </section>
