0e490929f61b7a3690f7d0d47f85eba16f9cffe9
bruhGrassi <brunagm88@gmail.com>
2024-07-18 12:38:10 -0300

    chore: improve hook

--- src/hooks/useFetchWeather.js
+++ src/hooks/useFetchWeather.js
@@ -10,60 +10,51 @@ const useFetchWeather = (initialLocation = "London") => {
   const [unit, setUnit] = useState(UNITS.CELCIUS);
   const [isLoading, setIsLoading] = useState(false);
 
+  const generateUrl = (location, type, apiKey) => {
+    const baseEndpoint =
+      type === WEATHER_TYPES.CURRENT ? API.CURRENT_URL : API.FORECAST_URL;
+    let url = "";
+
+    if (typeof location === "string") {
+      url = `${baseEndpoint}?q=${location}&appid=${apiKey}&units=metric`;
+    } else if (typeof location === "object" && location.lat && location.lon) {
+      url = `${baseEndpoint}?lat=${location.lat}&lon=${location.lon}&appid=${apiKey}&units=metric`;
+    } else {
+      throw new Error("Invalid location format");
+    }
+
+    return url;
+  };
+
   const fetchWeather = async (location, type) => {
     setError("");
+    setIsLoading(true);
 
     try {
-      let url, mappedDataFn, setDataFn;
-
-      setIsLoading(true);
-
-      if (type === WEATHER_TYPES.CURRENT) {
-        if (typeof location === "string") {
-          url = `${API.CURRENT_URL}?q=${location}&appid=${API_KEY}&units=metric`;
-        } else if (
-          typeof location === "object" &&
-          location.lat &&
-          location.lon
-        ) {
-          url = `${API.CURRENT_URL}?lat=${location.lat}&lon=${location.lon}&appid=${API_KEY}&units=metric`;
-        } else {
-          throw new Error("Invalid location format");
-        }
-        mappedDataFn = mapCurrentWeatherData;
-        setDataFn = setCurrentWeatherData;
-      } else if (type === WEATHER_TYPES.FORECAST) {
-        if (typeof location === "string") {
-          url = `${API.FORECAST_URL}?q=${location}&appid=${API_KEY}&units=metric`;
-        } else if (
-          typeof location === "object" &&
-          location.lat &&
-          location.lon
-        ) {
-          url = `${API.FORECAST_URL}?lat=${location.lat}&lon=${location.lon}&appid=${API_KEY}&units`;
-        } else {
-          throw new Error("Invalid location format");
-        }
-        mappedDataFn = mapForecastData;
-        setDataFn = setForecastWeatherData;
-      } else {
-        throw new Error("Invalid fetch type");
-      }
-
+      const apiKey = API_KEY;
+      const url = generateUrl(location, type, apiKey);
       const response = await fetch(url);
+
       if (!response.ok) {
         throw new Error("City not found");
       }
 
       const data = await response.json();
-      const mappedData = mappedDataFn(data);
-      setDataFn(mappedData);
+      const mappedData =
+        type === WEATHER_TYPES.CURRENT
+          ? mapCurrentWeatherData(data)
+          : mapForecastData(data);
+
+      if (type === WEATHER_TYPES.CURRENT) {
+        setCurrentWeatherData(mappedData);
+      } else if (type === WEATHER_TYPES.FORECAST) {
+        setForecastWeatherData(mappedData);
+      }
 
       return true;
     } catch (error) {
       console.error("Error fetching weather data:", error);
       setError(error.message);
-
       return false;
     } finally {
       setIsLoading(false);
