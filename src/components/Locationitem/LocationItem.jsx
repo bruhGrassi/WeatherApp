35e3f4f924b7b797a71949d884f657d2eff02c7e
bruhGrassi <brunagm88@gmail.com>
2024-07-23 19:51:25 -0300

    chore: Replace the styling of Locationitem with styled components

--- src/components/Locationitem/LocationItem.jsx
+++ src/components/Locationitem/LocationItem.jsx
@@ -1,13 +1,50 @@
 import PropTypes from "prop-types";
+import styled from "styled-components";
 import { ChevronRight } from "lucide-react";
-import "./LocationItem.css";
+
+const Item = styled.button`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-family: var(--font-family);
+  font-size: 1rem;
+  font-weight: var(--font-weight-regular);
+  line-height: var(--line-height-xsmall);
+  color: var(--color-tertiary);
+  padding: 0.75rem;
+  cursor: pointer;
+  border: 1px solid transparent;
+  transition: border 0.3s ease;
+  text-transform: uppercase;
+
+  svg {
+    width: 2rem;
+    opacity: 0;
+  }
+
+  &:hover {
+    border: 1px solid var(--color-tertiary);
+    transition: border 0.3s ease;
+    scale: 1;
+
+    & svg {
+      opacity: 1;
+    }
+  }
+
+  @media (max-width: 1023px) {
+    max-height: 35rem;
+  }
+`;
 
 const LocationItem = ({ text, handleLocation }) => {
   return (
-    <button className="location-item" onClick={handleLocation}>
+    <Item onClick={handleLocation}>
       {text}
       <ChevronRight />
-    </button>
+    </Item>
   );
 };
 
