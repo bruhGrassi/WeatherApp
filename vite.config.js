6e216940b4b0c579f77241de86960ce7c82a21da
bruhGrassi <brunagm88@gmail.com>
2024-07-23 18:41:07 -0300

    chore: Improve the debugging of styled components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import legacy from "@vitejs/plugin-legacy";
+
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react({ plugins: [["@swc/plugin-styled-components", {}]] }),
+    legacy(),
+  ],
+  define: {
+    "process.env.NODE_ENV": `'${mode || "production"}'`,
+    SC_DISABLE_SPEEDY: "true",
+  },
+}));
