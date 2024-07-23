b8990ae5e2e32cc49126536dd83e80d866caacc6
bruhGrassi <brunagm88@gmail.com>
2024-07-23 20:28:31 -0300

    chore: Replace the styling of LocationSearch with styled components

--- src/components/LocationSearch/LocationSearch.jsx
+++ src/components/LocationSearch/LocationSearch.jsx
@@ -1,9 +1,98 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 import { X, Search } from "lucide-react";
 import LocationItem from "../Locationitem/LocationItem";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import "./LocationSearch.css";
+
+const LocationListWrapper = styled.div`
+  max-height: 90%;
+  overflow: auto;
+  padding-right: 1rem;
+  margin-top: var(--padding-size-medium);
+`;
+
+const LocationSearchWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  padding-top: var(--font-size-medium);
+  top: 0;
+  left: 0;
+  z-index: 100;
+  padding: var(--padding-size-medium);
+  background-color: var(--color-primary);
+  transform: translateX(-100%);
+  transition: all ease-in-out 0.4s;
+
+  ${(props) =>
+    props.isLocationSearchOpen &&
+    `
+      transform: translateX(0);
+    `}
+`;
+
+const CloseTrigger = styled.button`
+  background: transparent;
+  width: 1.084375rem;
+  position: absolute;
+  right: 2rem;
+  top: 2rem;
+  height: 2rem;
+  width: 2rem;
+  color: var(--color-action-primary);
+`;
+
+const LocationSearchForm = styled.form`
+  width: 100%;
+  display: flex;
+  gap: 0.5rem;
+  margin-top: 2.5rem;
+`;
+
+const commonHeight = css`
+  height: 3rem;
+`;
+
+const LocationSearchInputWrapper = styled.div`
+  ${commonHeight}
+  flex: 1;
+  position: relative;
+
+  & input {
+    width: 100%;
+    height: 100%;
+    border: 1px solid var(--color-tertiary);
+    background-color: var(--color-primary);
+    padding: 0 0.5rem 0 2.375rem;
+    color: var(--color-tertiary);
+    border-radius: 0.25rem;
+    outline: none;
+
+    &::placeholder {
+      color: var(--color-tertiary);
+    }
+  }
+
+  & svg {
+    position: absolute;
+    top: 50%;
+    left: 0.75rem;
+    width: 1.09375rem;
+    transform: translateY(-50%);
+    color: var(--color-action-primary);
+  }
+`;
+
+const LocationSearchTrigger = styled.button`
+  ${commonHeight}
+  width: 5.375rem;
+  background: var(--color-search);
+  color: var(--color-tertiary);
+  font-size: 1rem;
+  font-weight: var(--font-weight-regular);
+  line-height: var(--line-height-xsmall);
+`;
 
 const LocationSearch = ({
   error,
@@ -29,33 +118,26 @@ const LocationSearch = ({
   };
 
   return (
-    <div
-      className={`location-search ${
-        isLocationSearchOpen ? "location-search--active" : ""
-      }`}
-    >
-      <button
-        className="close__trigger"
-        onClick={handleLocationSearchVisibility}
-      >
+    <LocationSearchWrapper isLocationSearchOpen={isLocationSearchOpen}>
+      <CloseTrigger onClick={handleLocationSearchVisibility}>
         <X />
-      </button>
-      <form className="location-search__form" onSubmit={handleSearch}>
-        <div className="location-search__input">
+      </CloseTrigger>
+      <LocationSearchForm onSubmit={handleSearch}>
+        <LocationSearchInputWrapper>
           <input
             type="text"
             placeholder="search location"
             value={searchTerm}
             onChange={handleInputChange}
           />
-          <Search className="location-search__input--icon" />
-        </div>
-        <button className="location-search__trigger">Search</button>
-      </form>
+          <Search />
+        </LocationSearchInputWrapper>
+        <LocationSearchTrigger>Search</LocationSearchTrigger>
+      </LocationSearchForm>
 
       <ErrorMessage error={error} />
 
-      <div className="location__wrapper">
+      <LocationListWrapper>
         {locations.map((location, index) => (
           <LocationItem
             key={index}
@@ -63,8 +145,8 @@ const LocationSearch = ({
             handleLocation={() => handleLocationSearch(location)}
           />
         ))}
-      </div>
-    </div>
+      </LocationListWrapper>
+    </LocationSearchWrapper>
   );
 };
 
