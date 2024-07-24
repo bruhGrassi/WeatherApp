3adb10d073f1480d9a23617f1dc47edf25fbee23
bruhGrassi <brunagm88@gmail.com>
2024-07-24 12:43:28 -0300

    Replace the styling of WeatherCard with styled components

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,24 +1,63 @@
 import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 import { UNITS, ICON_URL } from "../../constants";
-import "./WeatherCard.css";
+
+const WeatherCardWrapper = styled.div`
+  background-color: var(--color-primary);
+  padding: 1.3125rem;
+`;
+
+const WeatherCardCommon = css`
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: var(--font-weight-medium);
+  line-height: var(--line-height-xsmall);
+`;
+
+const WeatherCardDate = styled.div`
+  ${WeatherCardCommon}
+  text-align: center;
+  color: var(--color-action-primary);
+`;
+
+const WeatherCardImage = styled.img`
+  width: 7.5rem;
+  margin: auto;
+`;
+
+const WeatherCardTemperature = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 0.75rem;
+`;
+
+const WeatherCardTemperatureMax = styled.div`
+  ${WeatherCardCommon}
+  color: var(--color-action-primary);
+`;
+
+const WeatherCardTemperatureMin = styled.div`
+  ${WeatherCardCommon}
+  color: var(--color-text-primary-light);
+`;
 
 const WeatherCard = ({ date, min_temp, max_temp, image, unit }) => {
   const iconUrl = `${ICON_URL}${image}@4x.png`;
   const currentUnit = unit === UNITS.CELSIUS ? UNITS.CELSIUS : UNITS.FAHRENHEIT;
 
   return (
-    <div className="weather-card">
-      <div className="weather-card__date">{date}</div>
-      <img src={iconUrl} className="weather-card__image" />
-      <div className="weather-card__temperature">
-        <div className="weather-card__temperature--max">
+    <WeatherCardWrapper>
+      <WeatherCardDate>{date}</WeatherCardDate>
+      <WeatherCardImage src={iconUrl} />
+      <WeatherCardTemperature>
+        <WeatherCardTemperatureMax>
           {max_temp}° {currentUnit}
-        </div>
-        <div className="weather-card__temperature--min">
+        </WeatherCardTemperatureMax>
+        <WeatherCardTemperatureMin>
           {min_temp}° {currentUnit}
-        </div>
-      </div>
-    </div>
+        </WeatherCardTemperatureMin>
+      </WeatherCardTemperature>
+    </WeatherCardWrapper>
   );
 };
 
