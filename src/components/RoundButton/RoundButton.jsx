fe1eed9ed6441f34b4b7eb2527db7179aaad27ea
bruhGrassi <brunagm88@gmail.com>
2024-07-24 11:43:14 -0300

    chore: Replace the styling of RoundButton with styled components

--- src/components/RoundButton/RoundButton.jsx
+++ src/components/RoundButton/RoundButton.jsx
@@ -1,17 +1,49 @@
 import PropTypes from "prop-types";
-import "./RoundButton.css";
+import styled, { css } from "styled-components";
 
 const RoundButton = ({ children, variant, isActive, onClick }) => {
-  const buttonClasses = [
-    "round-button",
-    `round-button__${variant}`,
-    isActive ? "round-button--active" : "",
-  ].join(" ");
+  const ButtonVariantes = {
+    primary: css`
+      font-family: var(--font-family);
+      font-size: var(--font-size-small);
+      font-weight: var(--font-weight-bold);
+      line-height: var(--line-height-small);
+      background: var(--color-action-secondary);
+      color: var(--color-action-primary);
+    `,
+    icon: css`
+      background-color: var(--color-action-ternary);
+      color: var(--color-action-primary);
+
+      & img {
+        width: 1.375rem;
+      }
+    `,
+  };
+
+  const Button = styled.button`
+    width: 2.5rem;
+    height: 2.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    box-shadow: var(--box-shadow);
+
+    ${(props) => ButtonVariantes[props.variant]}
+
+    ${(props) =>
+      props.isActive &&
+      css`
+        background: var(--color-tertiary);
+        color: var(--color-text-tertiary);
+      `}
+  `;
 
   return (
-    <button onClick={onClick} className={buttonClasses}>
+    <Button variant={variant} isActive={isActive} onClick={onClick}>
       {children}
-    </button>
+    </Button>
   );
 };
 
