03078056254b005eea989e29b131559ae9c280fe
bruhGrassi <brunagm88@gmail.com>
2024-07-24 16:31:44 -0300

    chore:separate the style to another file

--- src/components/RoundButton/RoundButton.jsx
+++ src/components/RoundButton/RoundButton.jsx
@@ -1,43 +1,5 @@
 import PropTypes from "prop-types";
-import styled, { css } from "styled-components";
-
-const ButtonVariantes = {
-  primary: css`
-    font-family: var(--font-family);
-    font-size: var(--font-size-small);
-    font-weight: var(--font-weight-bold);
-    line-height: var(--line-height-small);
-    background: var(--color-action-secondary);
-    color: var(--color-action-primary);
-  `,
-  icon: css`
-    background-color: var(--color-action-ternary);
-    color: var(--color-action-primary);
-
-    & img {
-      width: 1.375rem;
-    }
-  `,
-};
-
-const Button = styled.button`
-  width: 2.5rem;
-  height: 2.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  box-shadow: var(--box-shadow);
-
-  ${(props) => ButtonVariantes[props.$variant]}
-
-  ${(props) =>
-    props.$isActive &&
-    css`
-      background: var(--color-tertiary);
-      color: var(--color-text-tertiary);
-    `}
-`;
+import { Button } from "./styles";
 
 const RoundButton = ({ children, variant, isActive, onClick }) => {
   return (
---
+++ src/components/RoundButton/styles.js
@@ -0,0 +1,39 @@
+import styled, { css } from "styled-components";
+
+const ButtonVariantes = {
+  primary: css`
+    font-family: var(--font-family);
+    font-size: var(--font-size-small);
+    font-weight: var(--font-weight-bold);
+    line-height: var(--line-height-small);
+    background: var(--color-action-secondary);
+    color: var(--color-action-primary);
+  `,
+  icon: css`
+    background-color: var(--color-action-ternary);
+    color: var(--color-action-primary);
+
+    & img {
+      width: 1.375rem;
+    }
+  `,
+};
+
+export const Button = styled.button`
+  width: 2.5rem;
+  height: 2.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  box-shadow: var(--box-shadow);
+
+  ${(props) => ButtonVariantes[props.$variant]}
+
+  ${(props) =>
+    props.$isActive &&
+    css`
+      background: var(--color-tertiary);
+      color: var(--color-text-tertiary);
+    `}
+`;
