c836f2420b56276cf18b1bf99358c6066af9f440
bruhGrassi <brunagm88@gmail.com>
2024-07-24 16:10:02 -0300

    chore: separate the style to another file

--- src/components/Locationitem/LocationItem.jsx
+++ src/components/Locationitem/LocationItem.jsx
@@ -1,43 +1,6 @@
 import PropTypes from "prop-types";
-import styled from "styled-components";
+import { Item } from "./LocationItem";
 import { ChevronRight } from "lucide-react";
-
-const Item = styled.button`
-  width: 100%;
-  height: 4rem;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-family: var(--font-family);
-  font-size: 1rem;
-  font-weight: var(--font-weight-regular);
-  line-height: var(--line-height-xsmall);
-  color: var(--color-tertiary);
-  padding: 0.75rem;
-  cursor: pointer;
-  border: 1px solid transparent;
-  transition: border 0.3s ease;
-  text-transform: uppercase;
-
-  svg {
-    width: 2rem;
-    opacity: 0;
-  }
-
-  &:hover {
-    border: 1px solid var(--color-tertiary);
-    transition: border 0.3s ease;
-    scale: 1;
-
-    & svg {
-      opacity: 1;
-    }
-  }
-
-  @media (max-width: 1023px) {
-    max-height: 35rem;
-  }
-`;
 
 const LocationItem = ({ text, handleLocation }) => {
   return (
