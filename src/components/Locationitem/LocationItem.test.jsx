8439dc284f18effd0f1245733e20d2271bc0999f
bruhGrassi <brunagm88@gmail.com>
2024-07-25 17:04:33 -0300

    chore: add test to LocationItem component

---
+++ src/components/Locationitem/LocationItem.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import LocationItem from "./LocationItem";
+import { describe, it, expect, vi } from "vitest";
+
+describe("<LocationItem />", () => {
+  it("renders the location name", () => {
+    render(<LocationItem text="New York" />);
+    expect(screen.getByText("New York")).toBeInTheDocument();
+  });
+
+  it("calls handleLocation when clicked", () => {
+    //creates a mock function
+    const handleLocation = vi.fn();
+    render(<LocationItem text="New York" handleLocation={handleLocation} />);
+
+    //simulates a click on the item
+    fireEvent.click(screen.getByText("New York"));
+
+    expect(handleLocation).toHaveBeenCalledTimes(1);
+  });
+});
