703ead8f4b4bfe306d93d5a94f12d8090e626476
bruhGrassi <brunagm88@gmail.com>
2024-07-29 17:41:07 -0300

    chore: add test to TodayWeather component

---
+++ src/components/TodayWeather/TodayWeather.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import TodayWeather from "./TodayWeather";
+import { describe, it, expect } from "vitest";
+
+describe("<TodayWeather />", () => {
+  const props = {
+    temperature: 25,
+    description: "Sunny",
+    image: "01d",
+    unit: "C",
+    cityName: "New York",
+  };
+
+  it("renders TodayWeather component", () => {
+    render(<TodayWeather {...props} />);
+
+    expect(screen.getByText("Search for places")).toBeInTheDocument();
+    expect(screen.getByRole("img")).toBeInTheDocument();
+    expect(screen.getByText(25)).toBeInTheDocument();
+    expect(screen.getByText("°C")).toBeInTheDocument();
+    expect(screen.getByText("Sunny")).toBeInTheDocument();
+    expect(screen.getByText("New York")).toBeInTheDocument();
+  });
+});
