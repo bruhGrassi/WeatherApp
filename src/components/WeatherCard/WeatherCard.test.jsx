5607a95d7ab0c4ee66f74ae0fe4c8a9c6a76c64c
bruhGrassi <brunagm88@gmail.com>
2024-07-29 18:00:28 -0300

    chore: add test to WheatherCard component

---
+++ src/components/WeatherCard/WeatherCard.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import WeatherCard from "./WeatherCard";
+import { describe, it, expect } from "vitest";
+
+describe("<WeatherCard />", () => {
+  const props = {
+    date: "Tomorrow",
+    image: "01d",
+    max_temp: 29,
+    min_temp: 11,
+    unit: "C",
+  };
+
+  it("renders WeatherCrad component", () => {
+    render(<WeatherCard {...props} />);
+
+    expect(screen.getByText("Tomorrow")).toBeInTheDocument();
+    expect(screen.getByRole("img")).toBeInTheDocument();
+    expect(screen.getByText(/29°/)).toBeInTheDocument();
+    expect(screen.getByText(/11°/)).toBeInTheDocument();
+  });
+});
