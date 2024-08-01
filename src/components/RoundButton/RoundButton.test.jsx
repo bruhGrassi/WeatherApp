e877080d25eb110468bb405a33ae86eee66b5772
bruhGrassi <brunagm88@gmail.com>
2024-08-01 15:38:31 -0300

    chore: improve way of passing props

--- src/components/RoundButton/RoundButton.test.jsx
+++ src/components/RoundButton/RoundButton.test.jsx
@@ -2,24 +2,29 @@ import { describe, it, expect, vi } from "vitest";
 import { render, screen, fireEvent } from "@testing-library/react";
 import RoundButton from "./RoundButton";
 
+const renderRoundButton = (props = {}) => {
+  const defaultProps = {
+    variant: "primary",
+    isActive: true,
+    onClick: () => {},
+  };
+  return render(
+    <RoundButton {...defaultProps} {...props}>
+      Button Text
+    </RoundButton>
+  );
+};
+
 describe("<RoundButton />", () => {
   it("renders the button", () => {
-    render(
-      <RoundButton variant="primary" isActive={true} onClick={() => {}}>
-        Button Text
-      </RoundButton>
-    );
+    renderRoundButton();
 
     expect(screen.getByText("Button Text")).toBeInTheDocument();
   });
 
   it("call onClick when clicked", () => {
     const handleClick = vi.fn();
-    render(
-      <RoundButton variant="primary" isActive={true} onClick={handleClick}>
-        Button Text
-      </RoundButton>
-    );
+    renderRoundButton({ onClick: handleClick });
 
     fireEvent.click(screen.getByText("Button Text"));
 
