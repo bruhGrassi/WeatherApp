ccf378ef8c8963dc2922a2d34c63c63aabae3749
bruhGrassi <brunagm88@gmail.com>
2024-08-01 18:17:34 -0300

    chore: remove duplicate date

--- src/lib/get-daily-temperature.test.jsx
+++ src/lib/get-daily-temperature.test.jsx
@@ -10,20 +10,6 @@ const forecast = [
     image: "01d",
   },
   {
-    date: new Date("2024-01-08"),
-    dayOfWeek: "Monday",
-    temp_min: 12,
-    temp_max: 18,
-    image: "01d",
-  },
-  {
-    date: new Date("2024-01-09"),
-    dayOfWeek: "Tuesday",
-    temp_min: 8,
-    temp_max: 14,
-    image: "02d",
-  },
-  {
     date: new Date("2024-01-09"),
     dayOfWeek: "Tuesday",
     temp_min: 9,
@@ -52,8 +38,8 @@ describe("getDailyTemperature", () => {
     const result = getDailyTemperature(forecast);
 
     expect(result).toEqual([
-      { date: "Monday", temp_min: 10, temp_max: 18, image: "01d" },
-      { date: "Tuesday", temp_min: 8, temp_max: 15, image: "02d" },
+      { date: "Monday", temp_min: 10, temp_max: 15, image: "01d" },
+      { date: "Tuesday", temp_min: 9, temp_max: 15, image: "02d" },
       { date: "Wednesday", temp_min: 6, temp_max: 12, image: "03d" },
     ]);
   });
